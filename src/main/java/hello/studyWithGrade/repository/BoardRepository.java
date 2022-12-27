f9f75282baf4ae5565daa0cc4cdcdb45214803be
Eungyu-code <Agyu625@gmail.com>
2022-12-27 21:43:07 +0900

    refactor: 필요없는 annotation 삭제

--- src/main/java/hello/studyWithGrade/repository/BoardRepository.java
+++ src/main/java/hello/studyWithGrade/repository/BoardRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    List<Board> findByTitleLike(@Param("title") String title);
-    List<Board> findByUser(@Param("user") User user);
+    List<Board> findByTitleLike(String title);
+    List<Board> findByUser(User user);
 }
