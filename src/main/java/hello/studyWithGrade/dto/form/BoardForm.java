3bffd296199af94d4c6715eada6fc5dbde0e9254
Eungyu-code <Agyu625@gmail.com>
2022-12-28 21:35:51 +0900

    add: 게시글 작성 시에 사용할 폼 추가

---
+++ src/main/java/hello/studyWithGrade/dto/form/BoardForm.java
@@ -0,0 +1,12 @@
+package hello.studyWithGrade.dto.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class BoardForm {
+
+    private String title;
+    private String content;
+}
