48fba3444caca770a6cc4ffa006a7f125954d603
Eungyu-code <Agyu625@gmail.com>
2022-12-28 21:34:52 +0900

    add: 타인의 정보를 조회할 때 사용하는 DTO 추가

--- src/main/java/hello/studyWithGrade/dto/UserDto.java
+++ src/main/java/hello/studyWithGrade/dto/UserDto.java
@@ -3,23 +3,27 @@ package hello.studyWithGrade.dto;
 import hello.studyWithGrade.entity.user.User;
 import lombok.Getter;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+/*
+ * 타인의 정보를 조회할 때 사용하는 DTO
+ */
 @Getter
 public class UserDto {
 
+    private Long id;
     private String email;
-    private List<BoardDto> boardDtos;
-    private List<CommentDto> commentDtos;
+    private Double grade;
+    private Integer studies_ing;
+    private Integer studies_ended;
+
+    protected UserDto() {
+
+    }
 
     public UserDto(User user) {
+        this.id = user.getId();
         this.email = user.getEmail();
-        this.boardDtos = user.getBoards().stream()
-                .map(board -> new BoardDto(board))
-                .collect(Collectors.toList());
-        this.commentDtos = user.getComments().stream()
-                .map(comment -> new CommentDto(comment))
-                .collect(Collectors.toList());
+        this.grade = user.getGrade();
+        this.studies_ing = user.getStudies_ing();
+        this.studies_ended = user.getStudies_ended();
     }
 }
