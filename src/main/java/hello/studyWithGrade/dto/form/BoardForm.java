bc233fbba8b36a6f89fcb40e7caac76d3061753d
Eungyu-code <Agyu625@gmail.com>
2022-12-31 17:22:40 +0900

    add: keyword 변수 추가

--- src/main/java/hello/studyWithGrade/dto/form/BoardForm.java
+++ src/main/java/hello/studyWithGrade/dto/form/BoardForm.java
@@ -3,10 +3,13 @@ package hello.studyWithGrade.dto.form;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Setter
 @Getter
 public class BoardForm {
 
     private String title;
     private String content;
+    private List<String> keyword;
 }
