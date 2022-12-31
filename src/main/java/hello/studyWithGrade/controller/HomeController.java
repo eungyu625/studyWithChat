eb847cf77079e38625ec1431198bd6de95a637ee
Eungyu-code <Agyu625@gmail.com>
2022-12-31 17:23:56 +0900

    add: 홈 화면에서의 게시글 목록에 대한 dto를 (BoardDto -> )MainDto로 변경

--- src/main/java/hello/studyWithGrade/controller/HomeController.java
+++ src/main/java/hello/studyWithGrade/controller/HomeController.java
@@ -1,13 +1,54 @@
 package hello.studyWithGrade.controller;
 
+import hello.studyWithGrade.dto.BoardDto;
+import hello.studyWithGrade.dto.MainDto;
+import hello.studyWithGrade.entity.Board;
+import hello.studyWithGrade.service.BoardService;
+import hello.studyWithGrade.service.CommentService;
+import hello.studyWithGrade.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
-    public void home() {
+    private final BoardService boardService;
+    private final UserService userService;
+    private final CommentService commentService;
 
+    @GetMapping("/")
+    public ResponseEntity<List<MainDto>> home(@RequestParam(value = "title", required = false) String title) {
+        List<MainDto> mainDtos = new ArrayList<>();
+        if (title == null || title.length() == 0) {
+            for (Board board : boardService.findAll()) {
+                Long id = board.getId();
+                String boardTitle = board.getTitle();
+                String username = board.getUser().getEmail();
+                LocalDateTime writeTime = board.getWriteTime();
+                Integer commentNumber = commentService.findByBoard(board).size();
+                mainDtos.add(new MainDto(id, boardTitle, username, writeTime, commentNumber));
+            }
+        } else {
+            for (Board board : boardService.findByTitleLike(title)) {
+                Long id = board.getId();
+                String boardTitle = board.getTitle();
+                String username = board.getUser().getEmail();
+                LocalDateTime writeTime = board.getWriteTime();
+                Integer commentNumber = commentService.findByBoard(board).size();
+                mainDtos.add(new MainDto(id, boardTitle, username, writeTime, commentNumber));
+            }
+        }
+
+        return ResponseEntity.ok(mainDtos);
     }
 }
---
+++ src/main/java/hello/studyWithGrade/dto/MainDto.java
@@ -0,0 +1,23 @@
+package hello.studyWithGrade.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class MainDto {
+
+    private Long id;
+    private String title;
+    private String username;
+    private LocalDateTime writeTime;
+    private Integer commentNumber;
+
+    public MainDto(Long id, String title, String username, LocalDateTime writeTime, Integer commentNumber) {
+        this.id = id;
+        this.title = title;
+        this.username = username;
+        this.writeTime = writeTime;
+        this.commentNumber = commentNumber;
+    }
+}
