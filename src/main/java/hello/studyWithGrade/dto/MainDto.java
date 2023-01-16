81291e40f7ec2c9a05a3e41d45a599712b3e1fbf
Eungyu-code <Agyu625@gmail.com>
2023-01-16 22:03:59 +0900

    add: progress 변수 추가

--- src/main/java/hello/studyWithGrade/dto/MainDto.java
+++ src/main/java/hello/studyWithGrade/dto/MainDto.java
@@ -13,16 +13,18 @@ public class MainDto {
     private String title;
     private UserDto userDto;
     private String writeTime;
+    private boolean progress;
     private Integer commentNumber = 0;
 
     protected MainDto() {
 
     }
 
-    public MainDto(Long id, String title, User user, LocalDateTime writeTime, Integer commentNumber) {
+    public MainDto(Long id, String title, User user, boolean progress, LocalDateTime writeTime, Integer commentNumber) {
         this.id = id;
         this.title = title;
         this.userDto = new UserDto(user);
+        this.progress = progress;
         this.writeTime = DateTimeFormatter.ofPattern("MM-dd HH:mm").format(writeTime);
         if (commentNumber > 0) {
             this.commentNumber = commentNumber;
