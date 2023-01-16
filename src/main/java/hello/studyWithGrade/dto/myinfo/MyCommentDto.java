b6c70616c9470894cf04398a6496be3bf85ec714
Eungyu-code <Agyu625@gmail.com>
2023-01-16 22:03:25 +0900

    add: boardId, boardTitle 변수 추가

--- src/main/java/hello/studyWithGrade/dto/myinfo/MyCommentDto.java
+++ src/main/java/hello/studyWithGrade/dto/myinfo/MyCommentDto.java
@@ -3,6 +3,8 @@ package hello.studyWithGrade.dto.myinfo;
 import hello.studyWithGrade.entity.Comment;
 import lombok.Getter;
 
+import java.time.format.DateTimeFormatter;
+
 /*
  * 내 댓글을 조회할 때 사용하는 DTO
  */
@@ -11,6 +13,9 @@ public class MyCommentDto {
 
     private Long id;
     private String content;
+    private Long boardId;
+    private String boardTitle;
+    private String writeTime;
 
     protected MyCommentDto() {
 
@@ -19,5 +24,8 @@ public class MyCommentDto {
     public MyCommentDto(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
+        this.boardId = comment.getBoard().getId();
+        this.boardTitle = comment.getBoard().getTitle();
+        this.writeTime = DateTimeFormatter.ofPattern("MM-dd HH:mm").format(comment.getWriteTime());
     }
 }
