c098e2884e77baef8a2dd7c47b27f195c609417b
Eungyu-code <Agyu625@gmail.com>
2023-01-16 22:02:13 +0900

    Add: 모집 여부에 따른 DTO 변수 설정을 parameter를 통해 결정하는 로직 추가

--- src/main/java/hello/studyWithGrade/controller/BoardController.java
+++ src/main/java/hello/studyWithGrade/controller/BoardController.java
@@ -8,9 +8,11 @@ import hello.studyWithGrade.dto.form.BoardForm;
 import hello.studyWithGrade.dto.form.CommentForm;
 import hello.studyWithGrade.entity.Board;
 import hello.studyWithGrade.entity.Comment;
+import hello.studyWithGrade.entity.Study;
 import hello.studyWithGrade.entity.user.User;
 import hello.studyWithGrade.service.BoardService;
 import hello.studyWithGrade.service.CommentService;
+import hello.studyWithGrade.service.StudyService;
 import hello.studyWithGrade.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -18,10 +20,7 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +31,7 @@ public class BoardController {
     private final UserService userService;
     private final BoardService boardService;
     private final CommentService commentService;
+    private final StudyService studyService;
 
     @GetMapping("/board")
     public String writeForm(Model model) {
@@ -56,20 +56,31 @@ public class BoardController {
             result.rejectValue("keyword", "required", null, null);
         }
 
+        if (!StringUtils.hasText(boardForm.getStudyName())) {
+            result.rejectValue("studyName", "required", null, null);
+        }
+
         if (result.hasErrors()) {
             return "boards/boardForm";
         }
 
         User user = userService.findByEmail(sessionUser.getEmail());
         Board board = new Board();
-        board.create(boardForm.getTitle(), boardForm.getContent(), user, List.of("new"));
+        Study study = new Study();
+        study.create(boardForm.getStudyName(), user);
+        board.create(boardForm.getTitle(), boardForm.getContent(), user, boardForm.getKeyword(), study);
         boardService.write(board);
 
         return "redirect:/";
     }
 
     @GetMapping("/board/{boardId}")
-    public String getBoard(@PathVariable("boardId") Long boardId, Model model, @LoginUser SessionUser sessionUser) {
+    public String getBoard(@PathVariable("boardId") Long boardId, Model model, @LoginUser SessionUser sessionUser, @RequestParam(value = "모집완료", required = false) String progress) {
+
+        if (StringUtils.hasText(progress)) {
+            boardService.finishRecruiting(boardService.findById(boardId));
+            return "redirect:/board/" + boardId;
+        }
 
         Board board = boardService.findById(boardId);
         List<Comment> comments = commentService.findByBoard(board);
@@ -78,5 +89,26 @@ public class BoardController {
         model.addAttribute("userDto", new UserDto(userService.findByEmail(sessionUser.getEmail())));
 
         return "boards/board";
+    }
+
+    @PostMapping("/board/{boardId}")
+    public String recruiting(@PathVariable("boardId") Long boardId, @Validated @ModelAttribute CommentForm commentForm,
+                             BindingResult result, @LoginUser SessionUser sessionUser) {
+
+        if (!StringUtils.hasText(commentForm.getContent())) {
+            result.rejectValue("content", "required", null, null);
+        }
+
+        if (result.hasErrors()) {
+            return "boards/boardForm";
+        }
+
+        Board board = boardService.findById(boardId);
+        User user = userService.findByEmail(sessionUser.getEmail());
+        Comment comment = new Comment();
+        comment.create(commentForm.getContent(), board, user);
+        commentService.write(comment);
+
+        return "redirect:/board/" + boardId;
     }
 }
