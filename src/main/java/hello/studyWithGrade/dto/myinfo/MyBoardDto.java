af0f3b0ae7638c3b3b1548a881938f7d15596265
Eungyu-code <Agyu625@gmail.com>
2023-01-16 22:03:41 +0900

    add: progress 변수 추가

--- src/main/java/hello/studyWithGrade/dto/myinfo/MyBoardDto.java
+++ src/main/java/hello/studyWithGrade/dto/myinfo/MyBoardDto.java
@@ -3,6 +3,8 @@ package hello.studyWithGrade.dto.myinfo;
 import hello.studyWithGrade.entity.Board;
 import lombok.Getter;
 
+import java.time.format.DateTimeFormatter;
+
 /*
  * 내 게시글을 조회할 때 사용하는 DTO
  */
@@ -11,15 +13,21 @@ public class MyBoardDto {
 
     private Long id;
     private String title;
-    private String content;
+    private String writeTime;
+    private boolean progress;
+    private Integer commentNumber = 0;
 
     protected MyBoardDto() {
 
     }
 
-    public MyBoardDto(Board board) {
+    public MyBoardDto(Board board, Integer commentNumber) {
         this.id = board.getId();
         this.title = board.getTitle();
-        this.content = board.getContent();
+        this.progress = board.isProgress();
+        this.writeTime = DateTimeFormatter.ofPattern("MM-dd HH:mm").format(board.getWriteTime());
+        if (commentNumber > 0) {
+            this.commentNumber = commentNumber;
+        }
     }
 }
