03593c5aff063e8c8ad32cde0334441fd7f92f7e
Eungyu-code <Agyu625@gmail.com>
2023-01-18 20:47:40 +0900

    add: List<Board>를 반환하는 findAll 메소드 추가

--- src/main/java/hello/studyWithGrade/repository/BoardRepository.java
+++ src/main/java/hello/studyWithGrade/repository/BoardRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Page<Board> findByTitleLike(String title, Pageable pageable);
     Page<Board> findByUser(User user, Pageable pageable);
     Page<Board> findAll(Pageable pageable);
+
+    List<Board> findAll();
 }
