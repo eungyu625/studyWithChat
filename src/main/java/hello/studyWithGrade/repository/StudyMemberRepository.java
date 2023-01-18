14d819ea24de5acfe24b98f34202fe555f6b53b7
Eungyu-code <Agyu625@gmail.com>
2023-01-18 20:46:36 +0900

    add: 페이징을 위해 Page 타입으로 반환하는 메소드 추가
    -findByUser
    -findByStudy

--- src/main/java/hello/studyWithGrade/repository/StudyMemberRepository.java
+++ src/main/java/hello/studyWithGrade/repository/StudyMemberRepository.java
@@ -3,6 +3,8 @@ package hello.studyWithGrade.repository;
 import hello.studyWithGrade.entity.Study;
 import hello.studyWithGrade.entity.manytomany.StudyMember;
 import hello.studyWithGrade.entity.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,4 +13,7 @@ public interface StudyMemberRepository extends JpaRepository<StudyMember, Long> 
 
     List<StudyMember> findByUser(User user);
     List<StudyMember> findByStudy(Study study);
+
+    Page<StudyMember> findByUser(User user, Pageable pageable);
+    Page<StudyMember> findByStudy(Study study, Pageable pageable);
 }
