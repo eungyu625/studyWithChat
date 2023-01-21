a60b33656647088745f4238640793c26f545e02b
Eungyu-code <Agyu625@gmail.com>
2023-01-21 20:17:34 +0900

    add: 평가된 스터디원과 아닌 스터디원을 각각 다른 리스트에 저장하는 로직 추가

--- src/main/java/hello/studyWithGrade/dto/StudyDto.java
+++ src/main/java/hello/studyWithGrade/dto/StudyDto.java
@@ -12,19 +12,21 @@ public class StudyDto {
 
     private Long id;
     private String name;
-    private UserDto owner;
+    private StudyMemberDto owner;
     private boolean progress;
-    private List<StudyMemberDto> studyMemberDtos;
+    private List<StudyMemberDto> notEstimatedMembers;
+    private List<StudyMemberDto> estimatedMembers;
 
     protected StudyDto() {
 
     }
 
-    public StudyDto(Study study, List<User> users) {
+    public StudyDto(Study study, List<User> notEstimatedMembers, List<User> estimatedMembers) {
         this.id = study.getId();
         this.name = study.getName();
-        this.owner = new UserDto(study.getUser());
+        this.owner = new StudyMemberDto(study.getUser().getId(), study.getUser().getEmail());
         this.progress = study.isProgress();
-        this.studyMemberDtos = users.stream().map(user -> new StudyMemberDto(user.getId(), user.getEmail())).collect(Collectors.toList());
+        this.notEstimatedMembers = notEstimatedMembers.stream().map(studyMember -> new StudyMemberDto(studyMember.getId(), studyMember.getEmail())).collect(Collectors.toList());
+        this.estimatedMembers = estimatedMembers.stream().map(studyMember -> new StudyMemberDto(studyMember.getId(), studyMember.getEmail())).collect(Collectors.toList());
     }
 }
