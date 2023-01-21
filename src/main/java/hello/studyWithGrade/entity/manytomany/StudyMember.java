563bcee3db8a8a574072fd7c5f464be9e423fba9
Eungyu-code <Agyu625@gmail.com>
2023-01-21 20:14:20 +0900

    add: estimatedMembers 변수 추가

--- src/main/java/hello/studyWithGrade/entity/manytomany/StudyMember.java
+++ src/main/java/hello/studyWithGrade/entity/manytomany/StudyMember.java
@@ -1,11 +1,12 @@
 package hello.studyWithGrade.entity.manytomany;
 
 import hello.studyWithGrade.entity.Study;
-import hello.studyWithGrade.entity.estimate.EstimatedMember;
 import hello.studyWithGrade.entity.user.User;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * User와 Study Entity 간의 ManyToMany 관계를 해결하기 위한 클래스
@@ -25,8 +26,8 @@ public class StudyMember {
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private User user;
 
-    @Embedded
-    private EstimatedMember estimatedMember;
+    @ElementCollection
+    private List<Long> estimatedMembers;
 
     protected StudyMember() {
 
@@ -35,5 +36,9 @@ public class StudyMember {
     public StudyMember(Study study, User user) {
         this.study = study;
         this.user = user;
+    }
+
+    public void estimated(User user) {
+        estimatedMembers.add(user.getId());
     }
 }
