1f68025491ea22b0f6ec0f9424d46502cbefe4c6
Eungyu-code <Agyu625@gmail.com>
2023-01-24 22:51:24 +0900

    add: 모집완료 시에 사용자 정보에 진행중인 스터디 하나를 추가하는 로직 추가

--- src/main/java/hello/studyWithGrade/controller/BoardController.java
+++ src/main/java/hello/studyWithGrade/controller/BoardController.java
@@ -9,12 +9,17 @@ import hello.studyWithGrade.dto.form.CommentForm;
 import hello.studyWithGrade.entity.Board;
 import hello.studyWithGrade.entity.Comment;
 import hello.studyWithGrade.entity.Study;
+import hello.studyWithGrade.entity.manytomany.StudyMember;
 import hello.studyWithGrade.entity.user.User;
 import hello.studyWithGrade.service.BoardService;
 import hello.studyWithGrade.service.CommentService;
 import hello.studyWithGrade.service.StudyService;
 import hello.studyWithGrade.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -22,6 +27,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -69,24 +76,35 @@ public class BoardController {
         Study study = new Study();
         study.create(boardForm.getStudyName(), user);
         studyService.create(study);
-        board.create(boardForm.getTitle(), boardForm.getContent(), user, boardForm.getKeyword(), study);
+        List<String> keywords = new ArrayList<>();
+        for (String keyword : boardForm.getKeyword()) {
+            keywords.add(keyword.split("\"")[3]);
+        }
+        board.create(boardForm.getTitle(), boardForm.getContent(), user, keywords, study);
         boardService.write(board);
 
         return "redirect:/";
     }
 
     @GetMapping("/board/{boardId}")
-    public String getBoard(@PathVariable("boardId") Long boardId, Model model, @LoginUser SessionUser sessionUser, @RequestParam(value = "모집완료", required = false) String progress) {
+    public String getBoard(@PathVariable("boardId") Long boardId, @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                           Model model, @LoginUser SessionUser sessionUser, @RequestParam(value = "모집완료", required = false) String progress) {
 
         if (StringUtils.hasText(progress)) {
             boardService.finishRecruiting(boardService.findById(boardId));
             studyService.start(boardService.findById(boardId).getStudy());
+
+            for (StudyMember studyMember : boardService.findById(boardId).getStudy().getStudyMembers()) {
+                userService.start_study(studyMember.getUser());
+            }
+
             return "redirect:/board/" + boardId;
         }
 
         Board board = boardService.findById(boardId);
-        List<Comment> comments = commentService.findByBoard(board);
-        model.addAttribute("boardDto", new BoardDto(board, comments));
+        System.out.println(board.getKeyword().getClass().getSimpleName());
+        Page<Comment> comments = commentService.findByBoard(board, pageable);
+        model.addAttribute("boardDto", new BoardDto(board, comments, commentService.findByBoard(board).size()));
         model.addAttribute("commentForm", new CommentForm());
         model.addAttribute("userDto", new UserDto(userService.findByEmail(sessionUser.getEmail())));
 
@@ -95,6 +113,7 @@ public class BoardController {
 
     @PostMapping("/board/{boardId}")
     public String recruiting(@Validated @ModelAttribute CommentForm commentForm, BindingResult result,
+                             @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
                              @PathVariable("boardId") Long boardId, Model model, @LoginUser SessionUser sessionUser) {
 
         if (!StringUtils.hasText(commentForm.getContent())) {
@@ -102,7 +121,8 @@ public class BoardController {
         }
 
         if (result.hasErrors()) {
-            model.addAttribute("boardDto", new BoardDto(boardService.findById(boardId), commentService.findByBoard(boardService.findById(boardId))));
+            model.addAttribute("boardDto", new BoardDto(boardService.findById(boardId), commentService.findByBoard(boardService.findById(boardId), pageable),
+                    commentService.findByBoard(boardService.findById(boardId)).size()));
             model.addAttribute("userDto", new UserDto(userService.findByEmail(sessionUser.getEmail())));
             return "boards/board";
         }
