f4e2659a26206acf824e4d0fe27c2a69fc5e0fb1
Eungyu-code <Agyu625@gmail.com>
2023-01-24 22:57:59 +0900

    add: keyword를 통한 게시글 검색 메소드 추가

--- src/main/java/hello/studyWithGrade/controller/HomeController.java
+++ src/main/java/hello/studyWithGrade/controller/HomeController.java
@@ -1,6 +1,5 @@
 package hello.studyWithGrade.controller;
 
-import hello.studyWithGrade.dto.BoardDto;
 import hello.studyWithGrade.dto.MainDto;
 import hello.studyWithGrade.entity.Board;
 import hello.studyWithGrade.service.BoardService;
@@ -11,19 +10,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -34,10 +25,30 @@ public class HomeController {
     private final CommentService commentService;
 
     @GetMapping("/")
-    public String home(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
-                       @RequestParam(defaultValue = "1", required = true) String page, Model model) {
+    public String home(@PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                       @RequestParam(defaultValue = "1") String page,
+                       @RequestParam(value = "title", required = false) String title,
+                       @RequestParam(value = "keyword", required = false) String keyword, Model model) {
+
+        if (StringUtils.hasText(title)) {
+            Page<Board> boardPage = boardService.findByTitleContaining(title, pageable);
+            Page<MainDto> mainDtos = boardPage.map(board -> new MainDto(board.getId(), board.getTitle(), board.getUser(),
+                    board.isProgress(), board.getWriteTime(), commentService.findByBoard(board).size()));
+            model.addAttribute("mainDtos", mainDtos);
+            return "home";
+        }
+
+        if (StringUtils.hasText(keyword)) {
+            Page<Board> boardPage = boardService.findByKeyword(keyword, pageable);
+
+            Page<MainDto> mainDtos = boardPage.map(board -> new MainDto(board.getId(), board.getTitle(), board.getUser(),
+                    board.isProgress(), board.getWriteTime(), commentService.findByBoard(board).size()));
+            model.addAttribute("mainDtos", mainDtos);
+            return "home";
+        }
 
         Page<Board> boardPage = boardService.findAll(pageable);
+
         Page<MainDto> mainDtos = boardPage.map(board -> new MainDto(board.getId(), board.getTitle(), board.getUser(),
                 board.isProgress(), board.getWriteTime(), commentService.findByBoard(board).size()));
 
